a18d1ce5e3d48f7fb473f8dc7331656b9c636364
David Nagy <davidmnagy27@gmail.com>
2019-04-26 16:27:30 -0400

    modified index.php to use templete

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ require_once('vendor/autoload.php');
 
 //Create an instance of the Base class
 $f3 = Base::instance();
+$f3->set('colors', array('pink','green', 'blue'));
 
 // Turn on Fat-Free error reporting
 $f3->set('DEBUG', 3);
